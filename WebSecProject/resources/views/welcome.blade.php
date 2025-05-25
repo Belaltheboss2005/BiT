1ff41854297a10402873e4f9aa7f14254685d47c
Ahmed Ramadan <redoa7355@gmail.com>
2025-05-25 23:52:45 +0300

    testing push / pull

--- WebSecProject/resources/views/welcome.blade.php
+++ WebSecProject/resources/views/welcome.blade.php
@@ -23,7 +23,7 @@
             <div class="col-md-8">
                 <div class="card shadow-lg border-0">
                     <div class="card-body text-center bg-light">
-                        <h1 class="display-4 mb-3" style="font-weight: bold; color: #007bff;">Welcome to <span style="color: #ff5722;">hehe</span>!</h1>
+                        <h1 class="display-4 mb-3" style="font-weight: bold; color: #007bff;">Welcome to <span style="color: #ff5722;">BiT</span>!</h1>
                         <p class="lead mb-4">Discover, shop, and enjoy our electronics <span style="color: #ff5722; font-weight: bold;">BiT by BiT</span>.</p>
                         <a href="{{ route('products_list') }}" class="btn btn-lg btn-primary px-5 mb-3">Shop Now</a>
                     </div>
